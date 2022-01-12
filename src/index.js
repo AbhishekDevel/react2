f0c0f9f1030099826ce32c7d8fe76739bac7031d
AbhishekDevel <abhishek947122@gmail.com>
2022-01-12 16:52:39 +0530

    2 files modified, 3 added, 1 deleted

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
 
 import App from "./App";
+import Invoices from "./routes/invoices";
+import Expenses from "./routes/expenses"
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}/>
+      <Route path="/invoices" element={<Invoices />}/>
+      <Route path="/expenses" element={<Expenses />}/>
+    </Routes>
+  </BrowserRouter>, document.getElementById("root"));
